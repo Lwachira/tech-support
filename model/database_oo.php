1237149285f7cde6f27b81ae11dd303ead3cdf1c
Lincoln <blazeboy.wachira@gmail.com>
2016-09-21 18:32:58 +0200

    updated dsn with correct details

--- model/database_oo.php
+++ model/database_oo.php
@@ -1,6 +1,7 @@
 <?php
 class Database {
-    private static $dsn = 'mysql:host=us-cdbr-azure-central-a.cloudapp.net;dbname=acsm_12a760e9a58aea1';
+    
+    private static $dsn = 'mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=acsm_12a760e9a58aea1';
     private static $username = 'bc1bf7e6d41868';
     private static $password = 'cd92c7b7';
     private static $db;
