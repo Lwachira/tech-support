064bd3d922987cf25f9a657b4f9187b2c815bbc6
Lincoln <blazeboy.wachira@gmail.com>
2016-10-22 22:48:59 +0200

    trying to fix product register

---
+++ register_product/registration_confirmation.php
@@ -0,0 +1,11 @@
+<?php include("../view/header.php"); ?>
+
+<div class="section no-pad-bot">
+    <div class="container">
+        <h3>Register Product</h3>
+        <p>Product (<?php echo $product_code; ?>) was registered successfully.</p>
+    </div>
+</div>
+
+<?php include("../view/footer.php"); ?>
+
